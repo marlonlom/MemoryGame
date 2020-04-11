83be8ec8a29bce03d80db531c6a8346b2101e5b9
Alicia Beltran <aliceresponde@gmail.com>
2020-04-11 11:14:10 -0500

    lobby UI

---
+++ app/src/main/java/com/example/memorygame/BoardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board)
+    }
+}
---
+++ app/src/main/java/com/example/memorygame/LobbyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LobbyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lobby)
+    }
+}
